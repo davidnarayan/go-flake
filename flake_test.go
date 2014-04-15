57a67116e3e03838990272a454733faea48ff7ad
David Narayan <david_narayan@homedepot.com>
2014-04-15 00:19:32 -0400

    Add initial tests

---
+++ flake_test.go
@@ -0,0 +1,26 @@
+package flake
+
+import (
+	"testing"
+    "sort"
+)
+
+func TestNewFlake(t *testing.T) {
+	f, err := New()
+
+	if err != nil {
+		t.Errorf("Unable to create new ID generator: %s", err)
+	}
+
+    var ids []string
+
+	for i := 0; i < 4; i++ {
+		id := f.NextId()
+
+        ids = append(ids, id.String())
+	}
+
+    if !sort.StringsAreSorted(ids) {
+        t.Errorf("IDs are not k-sorted!")
+    }
+}
